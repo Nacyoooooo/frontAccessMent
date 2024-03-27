9b3cc70c1ed46b2253ac046138a2930ccef346a6
Nacyoooooo <111370767+Nacyoooooo@users.noreply.github.com>
2024-03-27 23:13:21 +0800

    修改Result类为Results，防止与Kotlin冲突

---
+++ src/main/kotlin/com/chenzhihao/frontassessment/Common/Results.kt
@@ -0,0 +1,18 @@
+package com.chenzhihao.frontassessment.Common
+
+import lombok.Data
+@Data
+class Results<T> internal constructor(val status: Int,
+                                      val msg: String,
+                                      val data: T) {
+    companion object{
+        val SUCCESS = 200
+        val FAIL = 500
+        fun ok(data: Any=Any(), msg: String="操作成功",status: Int= SUCCESS): Results<Any> {
+            return Results(status, msg, data)
+        }
+        fun fail(msg: String="操作失败",status: Int= FAIL,data: Any="null"): Results<Any> {
+            return Results(status, msg, data)
+        }
+    }
+}
